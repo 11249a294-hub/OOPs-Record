953d6ec158e57e941198c990dac943d3ea6f05ad
11249a294-hub <11249a294@kanchiuniv.ac.in>
2025-11-16 22:31:13 +0530

    Update 1. class objects and functions.cpp

--- 1. class objects and functions.cpp
+++ 1. class objects and functions.cpp
@@ -1,26 +1,22 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-class Student {
-public:
+class student
+{
+    public:
     string name;
-    int age;
-
-    void display() {
-        cout << "Name: " << name << ", Age: " << age << endl;
+    int rollno;
+    void display()
+    {
+        cout<<"name:"<<name
+            <<"roll no:"<<rollno<<endl;
     }
 };
-
-int main() {
-    Student s1;        
-    s1.name = "Alice";
-    s1.age = 20;
+int main()
+{
+    student s1;
+    s1.name="john";
+    s1.rollno=101;
+    cout<<"student details";
     s1.display();
-
-    Student s2;        
-    s2.name = "Bob";
-    s2.age = 22;
-    s2.display();
-
     return 0;
 }
